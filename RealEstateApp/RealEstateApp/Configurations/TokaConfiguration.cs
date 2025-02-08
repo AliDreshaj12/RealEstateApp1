d3515b51a58a4ff1c1193f1e130b4665fc6bf61d
AliDreshaj12 <151794718+AliDreshaj12@users.noreply.github.com>
2025-02-09 00:32:52 +0100

    Toka
    Krijimi i Tokes a

---
+++ RealEstateApp/RealEstateApp/Configurations/TokaConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+using RealEstateApp.Entities;
+
+namespace RealEstateApp.Configurations
+{
+    public class TokaConfigurations : IEntityTypeConfiguration<Toka>
+    {
+        public void Configure(EntityTypeBuilder<Toka> builder)
+        {
+            builder.HasBaseType<Prona>();
+            builder.Property(e => e.LandType).IsRequired().HasMaxLength(100);
+            builder.Property(e => e.Zona).IsRequired().HasMaxLength(100);
+            builder.Property(e => e.TopografiaTokes).IsRequired().HasMaxLength(100);
+            builder.Property(e => e.WaterSource).IsRequired();
+            builder.ToTable("Tokat");
+        }
+    }
+}
---
+++ RealEstateApp/RealEstateApp/Entities/Toka.cs
@@ -0,0 +1,10 @@
+﻿namespace RealEstateApp.Entities
+{
+    public class Toka : Prona
+    {
+        public string LandType { get; set; }
+        public string Zona { get; set; }
+        public string TopografiaTokes { get; set; }
+        public bool WaterSource { get; set; }
+    }
+}
