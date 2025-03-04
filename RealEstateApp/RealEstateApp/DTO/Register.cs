4e53ffda00766c0d631af50b81ec2b72d60e6e9e
AliDreshaj12 <151794718+AliDreshaj12@users.noreply.github.com>
2025-03-04 23:16:03 +0100

    LoginRegister

---
+++ RealEstateApp/RealEstateApp/DTO/Register.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RealEstateApp.DTO
+{
+    public class Register
+    {
+        [Required]
+        public string Username { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+    }
+}
---
+++ RealEstateApp/RealEstateApp/Entities/ApplicationUser.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace RealEstateApp.Entities
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public string Role { get; set; } = string.Empty;
+        public string RefreshToken { get; set; } = string.Empty;
+        public DateTime TokenCreated { get; set; }
+        public DateTime TokenExpires { get; set; }
+    }
+}
